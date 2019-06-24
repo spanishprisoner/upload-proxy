2fe6e14fa776c35735b9f3686000dd728f8f9eae
spanishprisoner <jacek.mowinski@gmail.com>
2019-06-24 21:08:53 +0200

    Non angualr frontend update

---
+++ src/UploadProxy.Front/Services/Implementation/HttpClientService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace UploadProxy.Front.Services.Implementation
+{
+	public class HttpClientService : IHttpClientService
+	{
+		private readonly HttpClient _httpClient;
+
+		public HttpClientService()
+		{
+			_httpClient = new HttpClient(new RetryHandler(new HttpClientHandler()));
+		}
+
+		public Task<HttpResponseMessage> Get(string url)
+		{
+			return _httpClient.GetAsync(new Uri(url));
+		}
+
+		public Task<HttpResponseMessage> Post(string url, HttpContent httpContent)
+		{
+			return _httpClient.PostAsync(new Uri(url), httpContent);
+		}
+	}
+}
