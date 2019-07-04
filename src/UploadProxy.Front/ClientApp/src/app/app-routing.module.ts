953fb49e1966a8102c04e1057dadc1a62c4e84f9
spanishprisoner <jacek.mowinski@gmail.com>
2019-07-04 20:32:27 +0200

    Restoring auth guard

--- src/UploadProxy.Front/ClientApp/src/app/app-routing.module.ts
+++ src/UploadProxy.Front/ClientApp/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './auth/auth-guard.service';
 import { FileUploadComponent } from './file-upload/file-upload.component';
 
 const routes: Routes = [
   {
     path: '',
     component: FileUploadComponent,
-    pathMatch: 'full'
+    pathMatch: 'full',
+    canActivate: [AuthGuard],
+    runGuardsAndResolvers: 'always'
   },
   { path: '**', redirectTo: '' }
 ];
