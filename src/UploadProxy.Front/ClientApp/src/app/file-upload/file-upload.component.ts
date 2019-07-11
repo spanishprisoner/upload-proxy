600583abef86364059f9ed3aa134a76e38a9d05a
spanishprisoner <jacek.mowinski@gmail.com>
2019-07-11 23:46:22 +0200

    File uploading feature

--- src/UploadProxy.Front/ClientApp/src/app/file-upload/file-upload.component.ts
+++ src/UploadProxy.Front/ClientApp/src/app/file-upload/file-upload.component.ts
@@ -4,24 +4,30 @@ import { HttpClient } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { ErrorType } from '../shared/error-type';
 import { of } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   templateUrl: './file-upload.component.html',
   styleUrls: ['./file-upload.component.css']
 })
 export class FileUploadComponent implements OnInit {
-  public uploader: FileUploader = new FileUploader({ url: '/' });
+  public uploader: FileUploader = new FileUploader({ url: '/api/upload', maxFileSize: 1024 * 1024 * 1024 });
   public hasBaseDropZoneOver = false;
   public hasAnotherDropZoneOver = false;
-  public accessError = 'Checking Access...';
+  public accessError = null;
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient, private authService: AuthService) {
   }
 
   ngOnInit(): void {
+    this.uploader.setOptions({
+      headers: [
+        { name: 'Authorization', value: 'Bearer ' + this.authService.getToken() }
+      ]
+    });
     this.uploader.onCompleteItem = (item: FileItem, response: string,
       status: Number, headers: ParsedResponseHeaders) => {
-      item.file.name += '`https://example.com/link_to_file';
+      item.file.name += '`' + response;
     };
 
     this.httpClient.get<void | ErrorType>('/api/auth/checkaccess')
